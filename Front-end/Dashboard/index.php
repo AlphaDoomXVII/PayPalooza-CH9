033dbb409497e316f315f5c82e0aaed92c4cdbc6
AlphaDoomXVII <514345@vistacollege.nl>
2023-03-23 16:35:47 +0100

    Dasboard Page (V 1.0.0)

---
+++ Front-end/Dashboard/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+
+    <link rel="stylesheet" href="style.css">
+    
+
+</head>
+<body>
+
+    <div id="balk"> <img src="../images/paypaloozaLogo.png" id="logo" >
+        <button class="balkBtn" id="Btn1" > <i class="fa-solid fa-house-chimney fa-bounce"></i> Dashboard </button>
+        <button class="balkBtn" id="Btn2" > <i class="fa-solid fa-money-bill-transfer fa-bounce"></i> Transacties </button>
+        <button class="balkBtn" id="Btn3" > <i class="fa-solid fa-money-bill fa-bounce"></i> Budget </button>
+        <button class="balkBtn" id="Btn4" > <i class="fa-solid fa-clipboard fa-bounce"></i> Categorie </button>
+    </div>
+
+    <h1 id="welkom">Welkom <?=$_SESSION['Firstname']?> <?=$_SESSION['Lastname']?> </h1>
+
+
+
+
+
+    <script src="https://kit.fontawesome.com/31e6b31a4b.js" crossorigin="anonymous"></script>
+
+</body>
+</html>

55c3034f4af314e2b1fc7473dcd4114c87def7ce
AlphaDoomXVII <514345@vistacollege.nl>
2023-03-30 10:53:15 +0200

    Transactions pagina css change + kleine fix

--- Front-end/Transaction/transactions.php
+++ Front-end/Transaction/transactions.php
@@ -19,64 +19,64 @@
         <a href="../Categorie"><button class="balkBtn" id="Btn4" > <i class="fa-solid fa-clipboard fa-bounce"></i> Categorie </button></a>
     </div>
 
-<div id="test">
-    <form action="transback.php" method="post" enctype="multipart/form-data">
+    <div id="listInsert">
+        <form action="transback.php" method="post" enctype="multipart/form-data">
 
-        <input type="text" name="Info" placeholder="Info" required="">
+            <input type="text" name="Info" placeholder="Info" required="" id="listInfo">
 
-        <input type="int" name="price" placeholder="prijs" required="">
+            <input type="int" name="price" placeholder="prijs" required="" id="listPrice">
 
-        <input type="int" name="description" placeholder="Bescrijving" required="">
-                
-        <button type="submit" name="submit" value="submit" class="button">ADD</button>
-    </form>
-</div>
-
-<div id="list">
-    <table class="table">
-            <thead>
-                <tr>
-                    <th>Info</th>
-                    <th>Price</th>
-                    <th>Date</th>
-                    <th>category</th>
-                    <th>Beschrijving</th>
-                </tr>
-            </thead>
-
-        <tbody>
-            <?php
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "paypalooza";
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                if($conn->connect_error){
-                    die("connection failed".$conn->connect_error);
-                }
-                echo"";
-
-                $sql = "SELECT * FROM transactions";
-                $result = $conn->query($sql);
-
-                if(!$result){
-                    die("invalid query: " . $conn->error);
-                }
-
-                while($row = $result->fetch_assoc()){
-                    echo"<tr>
-                    <td>" . $row["Info"] . "</td>
-                    <td>" . $row["Price"] . "</td>
-                    <td>" . $row["Date"] . "</td>
-                    <td>" . $row["category"] . "</td>
-                    <td>" . $row["discription"] . "</td>
+            <input type="int" name="description" placeholder="Bescrijving" required="" id="listDesc">
                     
-                    </tr>";
-                }
-            ?>
-        </tbody>
-    </table>
-</div>
+            <button type="submit" name="submit" value="submit" class="button" id="listBtn">ADD</button>
+        </form>
+    </div>
+
+    <div id="list">
+        <table class="table">
+                <thead>
+                    <tr>
+                        <th>Info</th>
+                        <th>Price</th>
+                        <th>Date</th>
+                        <th>category</th>
+                        <th>Beschrijving</th>
+                    </tr>
+                </thead>
+
+            <tbody>
+                <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "paypalooza";
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    if($conn->connect_error){
+                        die("connection failed".$conn->connect_error);
+                    }
+                    echo"";
+
+                    $sql = "SELECT * FROM transactions";
+                    $result = $conn->query($sql);
+
+                    if(!$result){
+                        die("invalid query: " . $conn->error);
+                    }
+
+                    while($row = $result->fetch_assoc()){
+                        echo"<tr>
+                        <td>" . $row["Info"] . "</td>
+                        <td>" . $row["Price"] . "</td>
+                        <td>" . $row["Date"] . "</td>
+                        <td>" . $row["category"] . "</td>
+                        <td>" . $row["description"] . "</td>
+                        
+                        </tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 <script src="https://kit.fontawesome.com/31e6b31a4b.js" crossorigin="anonymous"></script>
 

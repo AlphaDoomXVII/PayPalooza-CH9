f10f8d3a31aac9521d83fbd2900afb9ef680f661
AlphaDoomXVII <514345@vistacollege.nl>
2023-04-06 11:38:49 +0200

    Kleine dashboard fixes + logout button start

--- Front-end/Dashboard/index.php
+++ Front-end/Dashboard/index.php
@@ -15,15 +15,17 @@ include('../Placeholder/logincheck.php')
 
 </head>
 <body>
+    
+    
 
     <div id="balk"> <img src="../images/paypaloozaLogo.png" id="logo" >
         <a href="../Dashboard"><button class="balkBtn" id="Btn1" > <i class="fa-solid fa-house-chimney fa-bounce"></i> Dashboard </button></a>
         <a href="../Transaction/transactions.php"><button class="balkBtn" id="Btn2" > <i class="fa-solid fa-money-bill-transfer fa-bounce"></i> Transacties </button></a>
         <a href="../Budget"><button class="balkBtn" id="Btn3" > <i class="fa-solid fa-money-bill fa-bounce"></i> Budget </button></a>
         <a href="../Categorie"><button class="balkBtn" id="Btn4" > <i class="fa-solid fa-clipboard fa-bounce"></i> Categorie </button></a>
+        
+        <h1 id="welkom" >Welkom <?=$_SESSION['FirstName']?> <?=$_SESSION['LastName']?> </h1>
     </div>
-    
-    <h1 id="welkom" >Welkom <?=$_SESSION['FirstName']?> <?=$_SESSION['LastName']?> </h1>
 
     <div id="budgetDash">
        <h1 id="budgetDashText">placeholder</h1>
@@ -78,6 +80,8 @@ include('../Placeholder/logincheck.php')
             </tbody>
         </table>
     </div>
+
+    <a href="../logout"> <button id="logoutBtn" > <i class="fa-solid fa-user-xmark"> </i></button> </a>
 
 
 
